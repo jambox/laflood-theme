c035dfc1d76ee17d6c735f871ec41382c346c153
madebycaliper <eli@madebycaliper.com>
2016-08-20 02:28:36 -0500

    All categories are "top-level" now. boom

--- partials/resource-category-list.php
+++ partials/resource-category-list.php
@@ -2,11 +2,13 @@
   <h3 class="resource-category-list--title">I'd like to contribute my...</h3>
   <ul class="resource-category-list">
     <?php
-    $cat = get_term_by('slug', 'ways-to-help', 'category');
-    $child_cats = get_child_categories( $cat->term_id );  
-    foreach( $child_cats as $cat ) :
+    $cats = get_terms( 'category', array('hide_empty' => false ) );
+    foreach( $cats as $cat ) :
+      if ( $cat->term_id == 1 ) continue;
     ?>
-      <li class="resource-tile offering col-lg-4 col-md-6 col-sm-6 col-xs-12"><a href="<?php echo get_term_link( $cat->term_id ); ?>" class="resource-tile--link btn btn-default"><?php echo $cat->name; ?></a></li>
+      <li class="resource-category col-lg-4 col-md-6 col-sm-6 col-xs-12">
+        <a href="<?php echo get_term_link( $cat->term_id ); ?>" class="resource-category--link btn btn-default"><?php echo $cat->name; ?></a>
+      </li>
     <?php endforeach; ?>
   </ul> 
 </div>
