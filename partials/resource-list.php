be78d6fd0f8aa23d619fbc7991e97e19acd79047
madebycaliper <eli@madebycaliper.com>
2016-08-19 23:01:54 -0500

    Front page changes

--- partials/resource-list.php
+++ partials/resource-list.php
@@ -6,7 +6,7 @@
     $child_cats = get_child_categories( $cat->term_id );  
     foreach( $child_cats as $cat ) :
     ?>
-      <li class="resource-tile offering col-md-4 col-sm-6 col-xs-12"><a href="<?php echo get_term_link( $cat->term_id ); ?>" class="resource-tile--link btn btn-default"><?php echo $cat->name; ?></a></li>
+      <li class="resource-tile offering col-lg-4 col-md-6 col-sm-6 col-xs-12"><a href="<?php echo get_term_link( $cat->term_id ); ?>" class="resource-tile--link btn btn-default"><?php echo $cat->name; ?></a></li>
     <?php endforeach; ?>
   </ul> 
 </div>
