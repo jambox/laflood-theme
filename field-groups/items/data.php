381c98810e4a0e71486f86a7096aacb127fd3b25
Gifford Nowland <hi@giffordnowland.com>
2016-08-20 00:47:22 -0500

    acf fast

---
+++ field-groups/items/data.php
@@ -0,0 +1,73 @@
+<?php 
+$group = array (
+  'key' => 'group_57b7ed6aa0d83',
+  'title' => 'Items',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'key' => 'field_57b7ed84dfef8',
+      'label' => 'Items',
+      'name' => 'items',
+      'type' => 'repeater',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => 
+      array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'collapsed' => 'field_57b7ede9dfef9',
+      'min' => '',
+      'max' => '',
+      'layout' => 'table',
+      'button_label' => '+ Add Item',
+      'sub_fields' => 
+      array (
+        0 => 
+        array (
+          'key' => 'field_57b7ede9dfef9',
+          'label' => 'Item',
+          'name' => 'item',
+          'type' => 'text',
+          'instructions' => '',
+          'required' => 0,
+          'conditional_logic' => 0,
+          'wrapper' => 
+          array (
+            'width' => '',
+            'class' => '',
+            'id' => '',
+          ),
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'maxlength' => '',
+        ),
+      ),
+    ),
+  ),
+  'location' => 
+  array (
+    0 => 
+    array (
+      0 => 
+      array (
+        'param' => 'taxonomy',
+        'operator' => '==',
+        'value' => 'category',
+      ),
+    ),
+  ),
+  'menu_order' => 0,
+  'position' => 'normal',
+  'style' => 'default',
+  'label_placement' => 'top',
+  'instruction_placement' => 'label',
+  'hide_on_screen' => '',
+  'active' => 1,
+  'description' => '',
+);
