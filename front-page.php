0097bf248dff9426c86955260a9496275b270638
madebycaliper <eli@madebycaliper.com>
2016-08-22 00:07:23 -0500

    WIP add searchform

--- front-page.php
+++ front-page.php
@@ -7,14 +7,19 @@
         <h2>Thank you for your effort towards Louisiana's unified recovery.</h2>
         <p>This is a living directory of ways you can help the people and families that have been affected by the August 2016 flood disaster.</p>
 
-        <h3>All types of contributions are helpful, but <a href="<?php echo get_category_link_by_slug('funds'); ?>">Funds</a> and <a href="<?php echo get_category_link_by_slug('food-and-goods'); ?>">Bulk Goods</a> provide the biggest impact for the affected areas.</h3>
-        <p>Choose from the list below to find out how you can share your goods and services.</p>
+        <h3>All types of contributions are helpful, but <a href="<?php echo get_category_link_by_slug('funds'); ?>">Funding</a> will provide the biggest impact for the affected areas.</h3>
+        <p>If you'd like to help, choose from one of the following options:</p>
       </div>
     </div>
 
     <div class="row-fluid">
       <?php get_template_part('partials/resource-category-list'); ?>
     </div>
+
+    <div class="row-fluid">
+      <?php get_template_part('partials/search-bar'); ?>
+    </div>
+
   </div>
 <?php endwhile; ?>
 
