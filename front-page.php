d2ce88f4744d789a50f1eda889470efe0a76e782
Gifford Nowland <hi@giffordnowland.com>
2016-08-24 03:06:38 -0500

    added dynamic front-page

--- front-page.php
+++ front-page.php
@@ -1,11 +1,29 @@
 <?php get_header(); ?>
+<?php
+	// Dynamically display top-level Categories by category visitor type.
+	$args = array(
+		'hide_empty' => false,
+		'parent'=>0
+	);
+	$cats = get_terms('category', $args);
+	if( !empty($cats) ){
+		$services = [];
+		foreach ( $cats as $cat ) {
+			$visitor_types = get_field('cat_visitor_type', $cat);
+			foreach ( $visitor_types as $visitor_type ) {
+				$services[$visitor_type][] = $cat;
+			}
+		}
+	}
 
-<?php while (have_posts()) : the_post(); ?>
+	foreach ( $services as $service => $cat_obj ) {
+		echo '<h2>', esc_attr($service), '.</h2>';
+		echo '<p>', $service == 'I need help' ? 'Find ' : 'Give ';
+		foreach ( $cat_obj as $cat ) {
+			echo '<a href="#">', strtolower($cat->name), '</a>, ';
+		}
+		echo 'or <a href="#">something else</a>.</p>';
+	}
 
-	  <section class="page-content--wrap">
-	    <div class="page-content"><?php the_content(); ?></div>
-	  </section>
-
-<?php endwhile; ?>
-
+?>
 <?php get_footer(); ?>
