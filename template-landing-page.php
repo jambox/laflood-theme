037034365513ee696544c7ead2886db27920a0d4
Gifford Nowland <hi@giffordnowland.com>
2016-08-24 00:00:56 -0500

    separated form from function

--- template-landing-page.php
+++ template-landing-page.php
@@ -8,17 +8,11 @@
 <?php while (have_posts()) : the_post(); ?>
   <div class="front-page-wrap row-fluid">
     <div class="intro--wrap col-md-8 col-md-offset-2">
-      <div class="intro">
-        <h2>Thank you for your effort towards Louisiana's unified recovery.</h2>
-        <p>This is a living directory of ways you can help the people and families that have been affected by the August 2016 flood disaster.</p>
-
-        <h3>All types of contributions are helpful, but <a href="<?php echo get_category_link_by_slug('funds'); ?>">Funding</a> will provide the biggest impact for the affected areas.</h3>
-        <p>If you'd like to help, choose from one of the following options:</p>
-      </div>
+      <div class="intro"><?php the_content() ?></div>
     </div>
 
     <div class="row-fluid">
-      <?php get_template_part('partials/volunteer-category-list'); ?>
+      <?php get_template_part('partials/category-list'); ?>
     </div>
 
     <div class="row-fluid">
