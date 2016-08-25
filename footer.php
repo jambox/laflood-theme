515e01bebc765fccd6dd9e179ae405dcbe1681fa
madebycaliper <eli@madebycaliper.com>
2016-08-25 02:34:02 -0500

    What thee fuck

--- footer.php
+++ footer.php
@@ -2,16 +2,19 @@
     </div><!-- /.content-wrap -->
 
     <?php
-    if( visitor_type() ) {
+    if( visitor_type() && !is_page('explore') ) {
       get_template_part('partials/components/search-module');
     }
     ?>
 
     <footer class="flex">
-      <div class="footer-msg-wrap flex-box">
-        <p>See something missing from this list? Submit an organization on the <a href="<?php permalink_by_title('Add An Organization'); ?>" class="submit-org-link">Submissions Page</a>.</p>
-        <p class="disclaimer">This site is not for profit. It was put together quickly to try to help and it's probably not perfect. We hope you find it useful.</p>
-
+      <div class="footer-wrap flex-box">
+        <div class="container">
+          <div class="row-fluid">
+            <div class="col-md-4"><p>This is a living directory of services that can assist the people and families who have been affected by the August 2016 flood disaster. <a href="<?php permalink_by_title('About'); ?>">Learn more.</a></p></div>
+            <div class="col-md-4"><p>See something missing from our lists? Please help us expand this directory by <a href="?php permalink_by_title('Add An Organization'); ?>">sharing a resource here</a>.</p></div>
+          </div>
+        </div>
       </div>
     </footer>
   </div> <!-- .offcanvas-wrap-inner -->
