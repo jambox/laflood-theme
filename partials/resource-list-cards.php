4625a33dea9266f00fdf689e2a8c95ae20cdf920
Gifford Nowland <hi@giffordnowland.com>
2016-08-24 22:28:42 -0500

    fixed permalink

--- partials/resource-list-cards.php
+++ partials/resource-list-cards.php
@@ -1,11 +1,10 @@
 <div class="col-md-12">
-  <h4 class="top-five-header">The <strong>TOP 5</strong> ways to give in this category are listed below:</h4>
   <ul class="resource-list">
   <?php if( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
     <li class="resource-card list-inline col-lg-12 col-md-12 col-sm-12 col-xs-12">
       <div class="resource-card--header flex-box">
-        <h4 class="resource-name"><a href="<?php the_field('url') ?>"><?php the_title(); ?></a></h4>
+        <h4 class="resource-name"><a href="<?php get_permalink() ?>"><?php the_title(); ?></a></h4>
       </div>
       <div class="resource-card--body">
         <?php the_field('description'); ?>
