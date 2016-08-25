113bdcd9070ae900a273b91279a480f9d3ca9403
madebycaliper <eli@madebycaliper.com>
2016-08-25 00:08:31 -0500

    Search bar changes

--- partials/components/search-module.php
+++ partials/components/search-module.php
@@ -3,7 +3,7 @@
     <div class="row-fluid">
 
       <div class="col-md-12">
-        <h3 class="search-bar-header">Do you have a specialized service you can provide?<br>Search below to connect to an organization that can put you to work.</h3>
+        <h3 class="search-bar-prompt">Do you have a specialized service you can provide? Search below to connect to an organization that can put you to work.</h3>
       </div>
     </div>
   </div>
