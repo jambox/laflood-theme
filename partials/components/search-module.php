991b584aad986accf77cfed2e6c987ecfb7e24c6
madebycaliper <eli@madebycaliper.com>
2016-08-25 22:22:15 -0500

    cant contain myself

--- partials/components/search-module.php
+++ partials/components/search-module.php
@@ -3,6 +3,8 @@
     <h5 class="search-bar-prompt">Do you have a specialized service you can provide? Search below to connect to an organization that can put you to work.</h5>
   </div>
   <div class="search-bar--wrap">
-    <?php get_search_form(); ?>
+    <div class="container">
+      <?php get_search_form(); ?>
+    </div>
   </div>
 </section>
