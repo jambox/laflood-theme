9adc1f0a134ce76b812739051da63eb9eacc9e78
madebycaliper <eli@madebycaliper.com>
2016-08-26 04:54:33 -0500

    Org Search Form

--- templates/template-new-org.php
+++ templates/template-new-org.php
@@ -15,7 +15,28 @@
 
 				<?php the_content(); ?>
 
+				<div class="org-search-form--wrap">
 				<?php
+					$form_object =  get_page_by_title( 'Organization Search', null, 'acf-field-group' );
+					$form_ID = $form_object->ID;
+
+					if( $form_ID ) {
+						acf_form(array(
+							'id'           => 'org-search-form',
+							'post_id'      => 'new_post',
+							'new_post'     => array(
+								'post_type'   =>  get_org_cpt_name(),
+								'post_status' => 'draft'
+							),
+							'field_groups' => array($form_ID),
+							'submit_value' => 'Add New Organization',
+							'updated_message' => 'Thank you for your submission',
+						));
+					}
+				?>
+				</div>
+				<?php
+
 					$form_object =  get_page_by_title( 'Organization Fields', null, 'acf-field-group' );
 					$form_ID = $form_object->ID;
 
