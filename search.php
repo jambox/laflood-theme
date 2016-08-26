5085acb34ed07332b1dbd3fd3cc6ea362201a4a5
madebycaliper <eli@madebycaliper.com>
2016-08-26 07:43:18 -0500

    SEARCH!!

--- search.php
+++ search.php
@@ -1,12 +1,10 @@
 <?php get_header(); ?>
-  <section id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
     <?php if ( have_posts() ) : ?>
-      <header class="page-header">
-        <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentysixteen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
-      </header>
+      <div class="col-md-8 col-md-offset-2">
+        <header class="page-header">
+          <h1 class="page-title"><?php printf( __( 'Results for: %s', 'twentysixteen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
+        </header>
+      </div>
       <?php include('archive.php') ?>
-      <?php endif; ?>
-    </main>
-  </section>
+    <?php endif; ?>
 <?php get_footer(); ?>
