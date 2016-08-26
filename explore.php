eb7b0aa26f0a5011b6f0bd85bc19c3739be57dd8
madebycaliper <eli@madebycaliper.com>
2016-08-26 06:03:47 -0500

    goof troop

--- explore.php
+++
@@ -1,4 +0,0 @@
-<?php get_header(); ?>
-EXPLORE!
-  <?php get_template_part('partials/components/search-module'); ?>
-<?php get_footer(); ?>
--- page-explore.php
+++ page-explore.php
@@ -2,10 +2,14 @@
 
   <?php while (have_posts()) : the_post(); ?>
     <div class="page-wrap row-fluid">
-      <div class="explore-page--content col-md-7">
-        <?php the_content(); ?>
+      <div class="explore-page--content col-md-8 col-md-offset-2">
+        <h1>Other Services</h1>
+        <?php $oppportunities_prompt = is_client_page() ? "find help" : "volunteer"; ?>
+        <?php $action_prompt = is_client_page() ? "explore" : "consider giving"; ?>
+        <p class="intro">A directory of other oppportunities to <?php echo $oppportunities_prompt; ?> will be available soon. In the meantime, please <?php echo $action_prompt; ?> in one of the following areas.</p>
+        <hr>
+        <ul class="main-cats-list"><?php echo lfr_main_cats(); ?></ul>
       </div>
-      <?php get_sidebar(); ?>
     </div>
   <?php endwhile; ?>
 
