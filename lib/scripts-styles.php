63660eb41624960f62c68fbcfe2aaf9e208da45e
madebycaliper <eli@madebycaliper.com>
2016-08-26 09:07:53 -0500

    get da scrip

--- lib/scripts-styles.php
+++ lib/scripts-styles.php
@@ -70,4 +70,20 @@ function laflood_admin_styles() {
 }
 add_action('admin_enqueue_scripts', 'laflood_admin_styles');
 
-?>
+
+function load_ga_script() {
+  ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-82872344-1', 'auto');
+    ga('send', 'pageview');
+
+  </script> 
+<?php
+}
+add_action('wp_head', 'load_ga_script',1,  10);
+
