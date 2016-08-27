ee83aebcc2a84bddf2c16dc9a593347bd51f235b
madebycaliper <eli@madebycaliper.com>
2016-08-27 18:44:46 -0500

    more minor tweekz

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
           <div class="row">
             <div class="col-md-4"><p>This is a living directory of services that can assist the people and families who have been affected by the August 2016 flood disaster. <a href="<?php permalink_by_title('About'); ?>">Learn more</a>.</p></div>
             <div class="col-md-4 col-md-offset-1"><p>See something missing from our lists? Please help us expand this directory by <a href="<?php permalink_by_title('Add An Organization'); ?>">adding an organization</a>.</p>
-            <p>If you see any problems with our organization info, please let <a href="mailto:info@lafloodrecovery.org">us know</a>.</p></div>
-            <div class="col-md-3">
+            <p>If you see any problems with our organization info, please <a href="mailto:info@lafloodrecovery.org">let us know</a>.</p></div>
+            <div class="col-md-2 col-md-offset-1">
               <nav class="nav-footer"><?php
                 if (has_nav_menu('footer_navigation')) {
                   wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav list-unstyled']);
