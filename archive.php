a286befc07aef62e59c4f19c2018a43e3de6cb14
madebycaliper <eli@madebycaliper.com>
2016-08-28 23:07:43 -0500

    make central column left align consistently

--- archive.php
+++ archive.php
@@ -8,7 +8,7 @@ $prompt = $queried_object->description;
 $guidelines = get_field('cat_guidelines', 'category_' . $queried_object_id );
 
 ?>
-<div class="col-md-8 col-md-offset-2">
+<div class="col-md-8">
 
 <?php if ( is_client_page() ): ?>
   <h1><?php echo $queried_object->name; ?></h1>
