663b76df1fe2b3aa0365c83d5dea924617b70cf9
Gifford Nowland <hi@giffordnowland.com>
2016-08-29 22:44:14 -0500

    fixed jshint warning

--- assets/js/org-modal.js
+++ assets/js/org-modal.js
@@ -146,7 +146,7 @@ function getFormData($form){
     var indexed_array = {};
 
     $.map(unindexed_array, function(n, i){
-        indexed_array[n['name']] = n['value'];
+        indexed_array[n.name] = n.value;
     });
 
     return indexed_array;
