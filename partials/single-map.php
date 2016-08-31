faeecb62a2611a0a48a1583759d29db5435c82de
Gifford Nowland <hi@giffordnowland.com>
2016-08-31 05:11:19 -0400

    added to map partial

--- partials/single-map.php
+++ partials/single-map.php
@@ -2,7 +2,11 @@
 
 	$location_array = get_field('org_location', $post);
 	if ( is_array($location_array) ) {
+		$adr = $location_array['address'];
 		$lat = $location_array['lat'];
 		$lng = $location_array['lng'];
+
+		// Google map will be injected here
+		echo '<div class="single-map"><span class="marker" data-lat="', $lat, '" data-lng="', $lng, '">', $adr, '</span></div>';
 	}
 
