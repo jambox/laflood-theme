a0e27d9c3916c79c98d75d3d5998db0452bbeef5
Gifford Nowland <hi@giffordnowland.com>
2016-08-31 11:33:07 -0400

    hear no evil, speak no evil

--- partials/single-map.php
+++ partials/single-map.php
@@ -7,6 +7,6 @@
 		$lng = $location_array['lng'];
 
 		// Google map will be injected here
-		echo '<div class="single-map"><span class="marker" data-lat="', $lat, '" data-lng="', $lng, '">', $adr, '</span></div>';
+		echo '<div class="single-map"><span class="marker" data-lat="', $lat, '" data-lng="', $lng, '" aria-hidden="true">', $adr, '</span></div>';
 	}
 
