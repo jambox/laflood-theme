d97a97966c298437ab677a847eaca2ba9d2c16d5
Gifford Nowland <hi@giffordnowland.com>
2016-08-31 14:29:39 -0400

    single page

--- single.php
+++ single.php
@@ -18,97 +18,133 @@
 		$org_main_phone = get_field('org_main_phone');
 		$org_main_email = get_field('org_main_email');
 		$org_contacts = get_field('org_contacts');
+
+    // Link to categories
+    // if( $org_category ) {
+    //   echo $org_category->name;
+    // }
 	?>
   <article <?php post_class(); ?>>
-    <header>
+    <header class="col-md-12">
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php if( $org_parent_org ): ?>
-    <h2><?php echo $org_parent_org->name; ?></h2>
-    <?php endif; ?>
+      <?php if( $org_parent_org ) {
+        echo '<h2>', $org_parent_org->name, '</h2>';
+      } ?>
     </header>
     <div class="entry-content col-md-8">
       <?php the_content(); ?>
     </div>
-    <div class="single-sidebar col-md-3">
-    <?php if( $org_services ): ?>
-      <ul>
-        <?php foreach( $org_services as $org_services_term ): ?>
-      <li><?php echo $org_services_term->name; ?></li>
-    <?php endforeach; ?>
-    </ul>
-    <?php endif; ?> 
-    <?php if( $org_category ): ?>
-      <?php echo $org_category->name; ?>
-      <?php endif; ?>
-      <?php if( $org_visitor_type ): ?>
-        <ul>
-          <?php foreach( $org_visitor_type as $org_visitor_type_term ): ?>
-        <li><?php echo $org_visitor_type_term->name; ?></li>
-      <?php endforeach; ?>
-      </ul>
-    <?php endif; ?> 
-      <?php if( $org_location ):?>
-        <?php echo $org_location['address']; ?>
-      <?php endif; ?> 
+    <div class="single-sidebar col-md-3 col-md-offset-1">
+    <?php
+      echo '<ul class="org-location list-unstyled">';
+    if( $org_location ) {
+      //$address = preg_replace('/[,]/', '<br>', $org_location['address'], 1);
+      $address_array = explode(',', $org_location['address']);
+      echo '<li>';
+      $i = 0;
+      foreach ($address_array as $address_line) {
+        echo $address_line;
+        if ($i == 1) {
+          echo ', ';
+        } else {
+          echo '<br>';
+        }
+        $i++;
+      }
+      echo '</li>';
+    }
 
-      <?php if( $org_handles_goods ):?>
-        <?php echo $org_location_type_label; ?>
-      <?php endif; ?>
+    if( $org_main_phone || $ $org_main_email) {
+      echo '<ul class="list-unstyled list-inline">';
+      if( $org_main_email ) {
+        echo '<li><a href="mailto:', $org_main_email, '">email</a></li>';
+      }
+      if ( $org_main_phone ) {
+        echo '&middot;<li><a href="tel:+1' . preg_replace('/[^\d+]/', '', $org_main_phone) . '">', $org_main_phone, '</a></li>';
+      }
+      echo '</ul>';
+    }
 
-      <?php if( $org_website ):?> 
-      <a href="<?php echo $org_website; ?>">Website</a>
-    <?php endif; ?>
+    if( $org_website || $org_location ) {
+      echo '<li><ul class="list-unstyled list-inline">';
+      if ( $org_website ) {
+        echo '<li><a href="', $org_website, '">Website</a></li>';
+      }
+      if ( $org_location ) {
+        echo '&middot;<li><a href="https://maps.google.com?q=', urlencode($org_location['address']), '" target="_blank">Map</a></li>';
+      }
+      echo '</ul></li>';
+    }
+    echo '</ul>'; // /org_location, org_main_phone, org_website
 
-    <?php if( $org_main_phone ):?>
-      <?php echo $org_main_phone; ?> 
-    <?php endif; ?>
+    if( have_rows('org_contacts') ) {
+      echo '<ul class="org-contacts list-unstyled">',
+           '<h3>Contact</h3>';
 
-    <?php if( $org_main_email ):?> 
-      <a href="mailto:<?php echo $org_main_email; ?>"><?php echo $org_main_email; ?></a>
-    <?php endif; ?> 
+      while ( have_rows('org_contacts') ) { the_row(); 
 
+        $org_contact_name = get_sub_field('org_contact_name');
+        $org_contact_title = get_sub_field('org_contact_title');
+        $org_contact_email = get_sub_field('org_contact_email');
+        $org_contact_phone = get_sub_field('org_contact_phone');
 
-    <?php if( have_rows('org_contacts') ): ?>
+        echo '<ul><li><ul class="list-unstyled">';
+        if ( $org_contact_name ) {
+          echo '<li class="org-contacts--name">',
+          $org_contact_name  ? '<span>' . $org_contact_name . '</span>' : '',
+          $org_contact_title ? ', <em>' . $org_contact_title . '</em>' : '',
+          '</li>';
+        }
+        if ( $org_contact_email || $org_contact_phone ) {
+          echo '<li class="org-contacts--details">',
+          $org_contact_email ? '<a href="' . $org_contact_email . '"><em>email</em></a>' : '',
+          $org_contact_phone ? '<a href="tel:+1' . preg_replace('/[^\d+]/', '', $org_contact_phone) . '">' . $org_contact_phone . '</a>' : '',
+          '</li>';
+        }
 
-      <div class="org-contacts">
+        echo '</ul></li></ul>';
+      } // endwhile
 
-      <?php while ( have_rows('org_contacts') ) : the_row(); 
+      echo '</ul>';
+    } // endif org_contacts
 
-              $org_contact_name = get_sub_field('org_contact_name');
-              $org_contact_title = get_sub_field('org_contact_title');
-              $org_contact_email = get_sub_field('org_contact_email');
-              $org_contact_phone = get_sub_field('org_contact_phone');
-          ?>    
-          <div class="org-contact">
-            <?php if( $org_contact_name ): ?> 
-              <?php echo $org_contact_name; ?>
-            <?php endif;?> 
-            <?php if( $org_contact_title ): ?> 
-              <?php echo $org_contact_title; ?>
-            <?php endif;?> 
-            <?php if( $org_contact_email ): ?> 
-              <a href="<?php echo $org_contact_email; ?>"><?php echo $org_contact_email; ?></a>
-            <?php endif;?> 
-            <?php if( $org_contact_phone ): ?> 
-              <?php echo $org_contact_phone; ?>
-            <?php endif;?> 
+    if( $org_services ) {
+      echo '<h3>Services Offered</h3>',
+           '<ul class="org-services">';
 
-          </div>
+      foreach( $org_services as $org_services_term ) {
+        $term_url = get_term_link($org_services_term, get_service_tax_name());
+        echo '<li>',
+          /* '<a href="', $term_url, '">', */
+             esc_attr($org_services_term->name),
+          /* '</a>', */
+             '</li>';
+      }
+      echo '</ul>';
+    } // endif org_services
 
-      <?php endwhile; ?>
-      
+    //if( $org_visitor_type ) {
+    //  echo '<ul>';
+    //  foreach( $org_visitor_type as $org_visitor_type_term ) {
+    //    echo '<li>', $org_visitor_type_term->name, '</li>';
+    //  }
+    //  echo '</ul>';
+    //}
 
-      </div>
-      
+    if( $org_handles_goods ) {
+      echo '<ul class="org-handles-goods list-inline">';
+      if ( $org_location_type == 'both') {
+        $choices = $org_location_type_object['choices'];
+        $both = array_pop($choices);
+        echo '<li>', implode( ' &amp; ', $choices ), "</li>";
+      } else {
+        echo  '<li>', $org_location_type_label, ' Only</li>';
+      }
+      echo '</ul>';
+    }
 
-          
-
-      <?php endif; ?>
-
-
-
+    ?>
     </div>
-
   </article>
 <?php endwhile; ?>
 
