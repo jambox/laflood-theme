529e04fbffdf4fd7c95704b28c1c83627b472f28
madebycaliper <eli@madebycaliper.com>
2016-09-01 02:31:19 -0500

    reformat empty search results page

--- search.php
+++ search.php
@@ -1,12 +1,13 @@
 <?php get_header(); ?>
-    <?php if ( have_posts() ) : ?>
-      <div class="col-md-8">
-        <header class="page-header">
-          <h1 class="page-title"><?php printf( __( 'Results for: %s', 'twentysixteen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
-        </header>
-      </div>
-      <?php get_template_part('partials/search-list'); ?>
-    <?php else : ?>
-    <div>Sorry we couldn't find any results for your search</div>
+<div class="col-md-8">
+  <header class="page-header">
+  <?php if ( have_posts() ) : ?>
+    <h1 class="page-title"><?php printf( __( 'Results for: %s', 'twentysixteen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
+  <?php get_template_part('partials/search-list'); ?>
+  <?php else : ?>
+    <h3><?php printf( __( "Sorry, we couldn't find any results for '%s'", 'twentysixteen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h3>
+    <p class="intro">Please try searching again or visit the <a href="<?php echo site_url() ?>">homepage</a> to browse through our services.</p>
   <?php endif; ?>
+  </header>
+</div>
 <?php get_footer(); ?>
