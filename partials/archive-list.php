e9d727d001cad8d36321dae6efaddd580d516392
Gifford Nowland <hi@giffordnowland.com>
2016-09-07 23:40:08 -0500

    custom sql to the rescue

--- partials/archive-list.php
+++ partials/archive-list.php
@@ -1,4 +1,6 @@
 <?php
+$visitor_type = visitor_type();
+$visitor_type_term = get_term_by('slug', $visitor_type, 'lfr_visitor_type');
 $queried_object = get_queried_object();
 $queried_object_id = $queried_object->term_id;
 $queried_object_tax = $queried_object->taxonomy;
@@ -14,9 +16,20 @@ if ( is_array($children) && count($children) > 0) : ?>
     <?php
     $subcat_list = array();
   	foreach ( $children as $child ) {
-  		$term = get_term( $child, $queried_object_tax );
-  		$parent = get_term( $term->parent, $queried_object_tax );
-      $subcat_list[] = '<li><a class="sub-cat-link" href="/' . visitor_type() . '/' . $parent->slug . '/' . $term->slug . '">' . $term->name . '</a></li>';
+      global $wpdb;
+      $result = $wpdb->get_var("
+        SELECT COUNT(*) FROM $wpdb->posts p
+        JOIN $wpdb->term_relationships tr ON p.ID = tr.object_id
+        JOIN $wpdb->term_relationships tr2 ON p.ID = tr2.object_id
+        WHERE ( tr.term_taxonomy_id = $child->term_id AND tr2.term_taxonomy_id = $visitor_type_term->term_id )
+        AND p.post_status = 'publish'
+        LIMIT 1
+      ");
+      if ( intval($result) > 0 ){
+        $term = get_term( $child, $queried_object_tax );
+        $parent = get_term( $term->parent, $queried_object_tax );
+        $subcat_list[] = '<li><a class="sub-cat-link" href="/' . visitor_type() . '/' . $parent->slug . '/' . $term->slug . '">' . $term->name . '</a></li>';
+      }
   	}
     echo implode('', $subcat_list);
     ?>
