3b18fc3a804bcadf4a46c6d31db31dc0ce81ba01
madebycaliper <eli@madebycaliper.com>
2016-09-07 21:30:06 -0500

    Remove code that only showed top level categories on the org_category ACF

--- lib/init.php
+++ lib/init.php
@@ -53,9 +53,3 @@ function laflood_custom_rewrite_rule() {
 }
 add_action('init', 'laflood_custom_rewrite_rule', 10, 0);
 
-// filter the taxonomy shown in the admin area to children of a specific parent
-function laflood_limit_acf_taxonomies( $args, $field ) {
-  $args['parent'] = 0;
-  return $args;
-}
-add_filter('acf/fields/taxonomy/wp_list_categories', 'laflood_limit_acf_taxonomies', 10, 2);
