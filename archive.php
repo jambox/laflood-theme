4aee36414d36403c326886031cc79c013d1f3ab2
Gifford Nowland <hi@giffordnowland.com>
2016-09-08 00:15:45 -0500

    added filter to archive get_terms $children for visitor_type()

--- archive.php
+++ archive.php
@@ -3,20 +3,20 @@
 get_header();
 
 $queried_object = get_queried_object();
-
 $queried_object_id = $queried_object->term_id; 
 $prompt = $queried_object->description;
 $guidelines = get_field('cat_guidelines', 'category_' . $queried_object_id );
 
+$visitor_type = visitor_type();
 ?>
 <div class="archive-list-header">
   <div class="col-md-6">
-    <?php if ( visitor_type() ): ?>
+    <?php if ( $visitor_type ): ?>
         <?php
           $term_parent = $queried_object->parent;
           if ( $term_parent > 0 ) {
             $parent = get_term($term_parent);
-            echo '<div class="sub-cat-breadcrumbs">Back to <a href="/', visitor_type(), '/', $parent->slug, '">', $parent->name, '</a></div>';
+            echo '<div class="sub-cat-breadcrumbs">Back to <a href="/', $visitor_type, '/', $parent->slug, '">', $parent->name, '</a></div>';
           } ?><h1 class="inline"><?php echo $queried_object->name; ?></h1>
     <?php endif ?>
 
@@ -52,7 +52,7 @@ $guidelines = get_field('cat_guidelines', 'category_' . $queried_object_id );
     'hide_empty' => true
   ) );
 
-  if ( is_array($children) && count($children) > 0) : ?>
+  if ( is_array($children) && count($children) > 0 && $visitor_type ) : ?>
     <ul class="sub-cat-list col-md-4 col-md-offset-2">
       <p>Choose from one of the categories below to refine your search:</p>
 
@@ -60,11 +60,19 @@ $guidelines = get_field('cat_guidelines', 'category_' . $queried_object_id );
         <h5 class="list-header">Types of <?php echo $queried_object->cat_name; ?></h5>
       <?php endif ?>
       <?php
+      $visitor_type_term = get_term_by('slug', $visitor_type, 'lfr_visitor_type');
       $subcat_list = array();
       foreach ( $children as $child ) {
+        global $wpdb;
+        $filtered_count = $wpdb->get_var(" SELECT COUNT(*) FROM $wpdb->posts p
+          JOIN $wpdb->term_relationships tr ON p.ID = tr.object_id JOIN $wpdb->term_relationships tr2 ON p.ID = tr2.object_id
+          WHERE ( tr.term_taxonomy_id = $child->term_id AND tr2.term_taxonomy_id = $visitor_type_term->term_id ) AND p.post_status = 'publish'
+          LIMIT 1 ");
+        if ( intval($filtered_count) > 0 ){
         $term = get_term( $child, $queried_object_tax );
         $parent = get_term( $term->parent, $queried_object_tax );
-        $subcat_list[] = '<li><a class="sub-cat-link" href="/' . visitor_type() . '/' . $parent->slug . '/' . $term->slug . '">' . $term->name . '</a></li>';
+        $subcat_list[] = '<li><a class="sub-cat-link" href="/' . $visitor_type . '/' . $parent->slug . '/' . $term->slug . '">' . $term->name . '</a></li>';
+        }
       }
       echo implode('', $subcat_list);
       ?>
