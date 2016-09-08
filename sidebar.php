6590f0e1e1298b004cc3c8e6b4b6f94772ad22a2
madebycaliper <eli@madebycaliper.com>
2016-09-08 16:16:06 -0500

    Change sidebar to only tell people not to ask for assistance...don't want to confuse the idea of volunteering FOR US

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
     <div class="page-sidebar col-md-4 col-md-offset-1">
-      <p>Please do not contact us directly about getting assistance or volunteering. If you have a general question for our team, email <a href="mailto:info@lafloodrecovery.org">info@lafloodrecovery.org</a>.</p>
+      <p>Please do not contact us directly about getting assistance. If you have a general question for our team, email <a href="mailto:info@lafloodrecovery.org">info@lafloodrecovery.org</a>.</p>
       <hr>
       <p>See something missing from our lists? Please help us expand this directory by <a href="<?php permalink_by_title('Add An Organization'); ?>">sharing a resource here</a>.</p>
       <hr>
