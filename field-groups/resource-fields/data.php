6b2f211f759864c85f0071cee1fdd0b8f6b22315
madebycaliper <eli@madebycaliper.com>
2016-09-07 21:37:27 -0500

    Add basic Resource ACFs

---
+++ field-groups/resource-fields/data.php
@@ -0,0 +1,107 @@
+<?php 
+$group = array (
+  'key' => 'group_57d0ce0f4e503',
+  'title' => 'Resource Fields',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'key' => 'field_57d0ce269782a',
+      'label' => 'Website',
+      'name' => 'resource_website',
+      'type' => 'url',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => 
+      array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'default_value' => '',
+      'placeholder' => '',
+    ),
+    1 => 
+    array (
+      'key' => 'field_57d0ce4f9782b',
+      'label' => 'Phone Number',
+      'name' => 'resource_phone',
+      'type' => 'text',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => 
+      array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'default_value' => '',
+      'placeholder' => '555-123-4567',
+      'prepend' => '',
+      'append' => '',
+      'maxlength' => '',
+    ),
+    2 => 
+    array (
+      'key' => 'field_57d0ce849782c',
+      'label' => 'Main Email',
+      'name' => 'resource_email',
+      'type' => 'email',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => 
+      array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'default_value' => '',
+      'placeholder' => '',
+      'prepend' => '',
+      'append' => '',
+    ),
+    3 => 
+    array (
+      'key' => 'field_57d0ce929782d',
+      'label' => 'Location',
+      'name' => 'resource_location',
+      'type' => 'google_map',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => 
+      array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'center_lat' => '30.39047',
+      'center_lng' => '-90.92800',
+      'zoom' => 11,
+      'height' => '',
+    ),
+  ),
+  'location' => 
+  array (
+    0 => 
+    array (
+      0 => 
+      array (
+        'param' => 'post_type',
+        'operator' => '==',
+        'value' => 'lfr_resource',
+      ),
+    ),
+  ),
+  'menu_order' => 0,
+  'position' => 'normal',
+  'style' => 'default',
+  'label_placement' => 'top',
+  'instruction_placement' => 'label',
+  'hide_on_screen' => '',
+  'active' => 1,
+  'description' => '',
+);
